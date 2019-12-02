dc381ede8b3f4bc63e64d47ec1d7e53364bd9570
TasseDeCafe <sebastien.stecker@gmail.com>
2019-12-02 19:04:51 +0400

    - created flask app

---
+++ db_to_csv.py
@@ -0,0 +1,72 @@
+import csv
+import os
+
+def get_list_books(cursor):
+    """ The list of book titles typed correctly is located in the table BOOK_INFO. This function returns a nest list.
+    The items of this list are lists that contain the book ids and the titles of the books that are printed to
+    the user. """
+    cursor.execute('SELECT id, title FROM BOOK_INFO')
+    all_books = cursor.fetchall()
+    return [[book_id, book_name] for (book_id, book_name) in all_books]
+
+
+def get_all_words_specific_book(cursor, book_id):
+    """ Knowing the book id, returns a nested list. The items of the list are lists that contain the highlighted words
+     in the book and the context sentence in which this word is found. """
+    book_id_tuple = (book_id,)
+    cursor.execute("SELECT word_key, usage FROM LOOKUPS WHERE book_key=?", book_id_tuple)
+    list_words_sentences = cursor.fetchall()
+    # This step removes the "en:" or "de:" before each word.
+    list_words_sentences_without_en = [[words[3:], sentences]
+                                       for [words, sentences] in list_words_sentences]
+    return list_words_sentences_without_en
+
+
+def main(cursor):
+    print("The list of books with highlighted content in your Kindle include the following books:")
+    my_books = get_list_books(cursor)
+    for book in my_books:
+        print(f"{my_books.index(book)}: {book[1]}")
+
+    print(f"Please type a number between 0 and {len(my_books) - 1} to generate the CSV file.")
+    user_input_number = int(input())
+    selected_book_id = my_books[user_input_number][0]
+
+    # Write the words and sentences in a CSV file.
+    with open("words_and_sentences.csv", "w") as words_and_sentences:
+        writer = csv.writer(words_and_sentences)
+        for line in get_all_words_specific_book(cursor, selected_book_id):
+            writer.writerow(line)
+
+
+def create_csv(cursor, application, book_index):
+    my_books = get_list_books(cursor)
+    selected_book_id = my_books[book_index][0]
+
+    # Write the words and sentences in a CSV file.
+    with open(os.path.join(application.instance_path, 'csv_files', f'words_and_sentences_{book_index}.csv'), "w") as words_and_sentences:
+        writer = csv.writer(words_and_sentences)
+        for line in get_all_words_specific_book(cursor, selected_book_id):
+            writer.writerow(line)
+
+
+# if __name__ == "__main__":
+#     """ This is executed when run from the command line """
+#     main(cursor)
+
+
+
+# def get_all_words_with_context():
+#     c.execute("SELECT word_key, usage FROM LOOKUPS")
+#     return c.fetchall()
+
+# for book in get_list_books():
+#     print(book)
+#
+# for word in get_all_words_specific_book('How_to_Change_Your_Mind:B7DE3B75'):
+#     print(word)
+#
+
+
+
+
---
+++ flask_app.py
@@ -0,0 +1,136 @@
+from flask import Flask, render_template, request, send_file, url_for, redirect, after_this_request, make_response, \
+    flash, Markup
+from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.utils import secure_filename
+import sqlite3
+from db_to_csv import *
+from flask_dropzone import Dropzone, _Dropzone
+import uuid
+import time
+import datetime
+import sys
+import threading
+
+app = Flask(__name__, template_folder='templates')
+app.debug = True
+app.config.update(
+    SECRET_KEY='CHANGEME',
+
+    # remove database after X seconds
+    DATABASE_PERSISTENCE=20,
+
+    # Flask-Dropzone config:
+    DROPZONE_ALLOWED_FILE_CUSTOM=True,
+    DROPZONE_ALLOWED_FILE_TYPE='.db',
+    DROPZONE_MAX_FILE_SIZE=50,
+    DROPZONE_MAX_FILES=1,
+    DROPZONE_REDIRECT_VIEW='book_list',
+    DROPZONE_DEFAULT_MESSAGE='Drop your Kindle database here or click to upload.',
+
+    # debugger config
+    DEBUG_TB_INTERCEPT_REDIRECTS=False
+)
+
+# create folder where files are uploaded
+os.makedirs(os.path.join(app.instance_path, 'htmlfi'), exist_ok=True)
+
+# used for debugging
+toolbar = DebugToolbarExtension(app)
+
+# create a dropzone
+dropzone = Dropzone(app)
+
+
+def style(css):
+    """Add css to dropzone.
+     :param css: style sheet code.
+    """
+    # return Markup('<style>\n.dropzone{%s}\n</style>' % css)
+    return Markup('<style>\n%s\n</style>' % css)
+
+
+# https://stackoverflow.com/questions/30640968/remove-old-temporary-files-when-user-is-logged-out
+
+class TimeSet(set):
+    def add(self, item, timeout):
+        set.add(self, item)
+        t = threading.Thread(target=timeout_set_remove, args=(self, item, timeout))
+        t.start()
+
+
+def timeout_set_remove(my_set, item, timeout):
+    time.sleep(timeout)
+    try:
+        os.remove(str(item))
+        my_set.remove(item)
+    except FileNotFoundError as error:
+        app.logger.error("Error removing database.", error)
+
+
+@app.route('/')
+def drop_file():
+    resp = make_response(render_template('drop_file.html', style=style))
+    resp.set_cookie("id", str(uuid.uuid4()))
+    return resp
+
+
+@app.route('/book_list', methods=["GET", "POST"])
+def book_list():
+    connection = sqlite3.connect(os.path.join(app.instance_path, 'htmlfi', f"{request.cookies['id']}.db"))
+    c = connection.cursor()
+    books = get_list_books(c)
+    return render_template('book_list.html', books=books)
+
+
+@app.route("/sendfile", methods=["GET", "POST"])
+def send_database_file():
+    f = request.files['file']
+    f.save(os.path.join(app.instance_path, 'htmlfi', secure_filename(f"{request.cookies['id']}.db")))
+    my_set = TimeSet()
+    my_set.add(os.path.join(app.instance_path, 'htmlfi', f"{request.cookies['id']}.db"),
+               app.config['DATABASE_PERSISTENCE'])
+
+    return redirect(url_for('book_list'))
+
+
+# idea: redirect to /to_csv_<filename> and open the CSV directly
+
+
+@app.route('/to_csv', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        user_book_index = int(request.form['book_id'])
+        try:
+            connection = sqlite3.connect(os.path.join(app.instance_path, 'htmlfi', f"{request.cookies['id']}.db"))
+            c = connection.cursor()
+            create_csv(c, app, user_book_index)
+            file_path = os.path.join(app.instance_path, 'csv_files', f'words_and_sentences_{user_book_index}.csv')
+            file_handle = open(file_path, 'r')
+        except (KeyError, sqlite3.OperationalError):
+            resp = redirect(url_for('drop_file'))
+            # remove the cookie by changing its max_age to zero
+            resp.set_cookie("id", "null", max_age=0)
+            flash('Your session has expired. Please reupload your database.')
+            return resp
+
+        @after_this_request
+        def remove_file(response):
+            try:
+                os.remove(file_path)
+                file_handle.close()
+            except Exception as error:
+                app.logger.error("Error removing or closing downloaded file handle", error)
+            return response
+
+        to_return = send_file(
+            os.path.join(app.instance_path, 'csv_files', f'words_and_sentences_{user_book_index}.csv'),
+            mimetype='text/csv',
+            conditional=False)
+        to_return.headers["Content-Disposition"] = f"attachment; filename='words_and_sentences_{user_book_index}.csv"
+        # to_return.headers["Access-Control-Expose-Headers"] = 'x-filename'
+        return to_return
+    # return send_file(filename_or_fp=os.path.join(app.instance_path, 'csv_files', 'words_and_sentences.csv'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
